8e6ca084b53528f275b72fd34db05566307dfc81
Pravin Ranjan <pravinranjan10@gmail.com>
2025-02-22 16:40:04 +0530

    two odd occuring

--- Bit-Magic/two_odd_occuring.py
+++ Bit-Magic/two_odd_occuring.py
@@ -1,28 +1,32 @@
-# Find two numbers which comes odd number of times
-
-
 def oddappearing(arr) :
     xors = 0
-    res1 = 0
-    res2 = 0
+    group1 = 0
+    group2 = 0
     
-    for i in arr :
-        xors = xors ^ i
+    for num in arr :
+        xors = xors ^ num   # Find xor of all element of array.
         
-    print("xor=", xors)
-    sn = xors & -(xors-1)
-    print("sn=", sn)
+    print("xor=", xors)  # For above example, xor would be (5^6)=3
+    sn = xors & ~(xors-1)  #  Find Rightmost set bit's of a number
+                           # Now question is why we want to find Rightmost set bit.
+                           # So finding RMS will tell that one of the no. has set bit at exactly same position and 
+                           # other number doesn;t has the set bit at exactly same position.
+    print("sn=", sn)       # 1
     
-    for i in arr :
-        if i & sn != 0 :
-            res1 = res1 ^ i
-        
+    for num in arr :    # Now devide whole array in two groups. group1: which will contains all number
+                        # which Rightmost set bit is 1(1st position)
+        if num & sn != 0 :
+            group1 = group1 ^ num    # group1={3, 3, 5, 7, 7}. So this group has all the element which 
+                                    # rightmost set bit position is 1
         else :
-            res2 = res2 ^ i 
+            group2 = group2 ^ num    # group2={4, 4, 4, 4, 6}. So this group has all the element which 
+                                    # rightmost set bit position is NOT 1
             
-    print(res1,res2)
+    print(group1,group2)
             
             
 if __name__ == "__main__" :
     arr = [3,4,3,4,5,4,4,6,7,7]
     oddappearing(arr)
+
+# Reference: https://www.youtube.com/watch?v=oHDyVK43sGc
